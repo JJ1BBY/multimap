5de2c3c9f2527fe02bf5f934328d20dc28a715d5
JJ1BBY <79028771+JJ1BBY@users.noreply.github.com>
2022-02-06 21:34:33 +0900

    Update multimap.py
    全市全郡と関東Uに対応するために正規表現で59xxxxxLと出力なしの両方を対応

--- multimap.py
+++ multimap.py
@@ -18,13 +18,19 @@ jccjcgdata = "multitable.csv"
 #logファイル読み込み
 df_log = pd.read_csv(
     logfile,
+    encoding = "shift-jis", 
     delim_whitespace=True,
     header=None, 
-    skiprows=2,
-    names=('No', 'Date', 'Time', 'CS', 'Band', 'Mode', 'His', 'My'),
+    names=('Date', 'Time', 'CS', 'Band', 'Mode', 'His', 'My', "Remarks"),
     index_col=0
 )
-df_log['My'] = df_log['My'].astype(str).str[2:]
+
+#ACAGなどパワーがMyについている場合は削除
+#CWとPhoneで599/59を削除、出力の行末を削除
+#astype(str)で文字列に
+df_log['My'] = df_log['My'].astype(str).replace('(^[1-5][1-9]9?)(\d.*?)([HLMP]?$)', r'\2', regex=True)
+print(df_log.columns)
+print(df_log)
 #Working DFとなるdf_Myを作成
 df_My =  pd.DataFrame()
 #ログファイルのMyの数を数えてユニークマルチの交信数としてcountに代入
@@ -32,11 +38,15 @@ df_My['Count'] = df_log['My'].value_counts()
 #indexを追加
 df_My['JCCJCGd'] = df_My.index
 df_My.reset_index(inplace=True, drop=True)
-
 #白地図のデータをファイルからraw stringで読み込み
 fp = r"D:\Downloads\N03-20210101_GML\N03-20210101_GML\N03-21_210101.shp"
 geo_data = gpd.read_file(fp, encoding="sjis")
-
+df_log.to_csv(
+        pathlib.Path("", "log1.csv"),
+        #index=False,
+        #quoting=csv.QUOTE_NONNUMERIC,
+        encoding="utf_8_sig"
+)
 #マスターデータを読み込み
 df_list = pd.read_csv(
     jccjcgdata,
@@ -46,28 +56,31 @@ df_list = pd.read_csv(
 )
 
 #~で重複する空白の値を含むレコードをdropして、一意のリストにする
+
 df_JCC = df_list[~df_list.duplicated(subset='JCCCd')]
 df_WD = df_list[~df_list.duplicated(subset='WDCd')]
 df_JCG = df_list[~df_list.duplicated(subset='JCGCd')]
 
+
 #df_My にJCC/WD(市区郡詳細コード)/JCGを追加し、全国の自治体コードmuniCdを転記する
 df_My.insert(2, 'JCCmuniCd', df_My['JCCJCGd'].map(df_JCC.set_index('JCCCd')['muniCd']))   
 df_My.insert(2, 'WDmuniCd',  df_My['JCCJCGd'].map(df_WD.set_index ('WDCd') ['muniCd']))   
-df_My.insert(3, 'JCGmuniCd', df_My['JCCJCGd'].map(df_JCG.set_index('JCGCd')['muniCd']))   
-#print(df_My)
+df_My.insert(2, 'JCGmuniCd', df_My['JCCJCGd'].map(df_JCG.set_index('JCGCd')['muniCd']))   
+
+
 
 #muniCdを追加後、JCCmuniCdの欠損に、WDとJCGの値を転記してマスターキーとする
 df_My['JCCmuniCd'].fillna(df_My['JCGmuniCd'], inplace=True)
 df_My['JCCmuniCd'].fillna(df_My['WDmuniCd'], inplace=True)
+#print(df_My['JCCmuniCd'][df_My['JCCmuniCd'].duplicated()])
 
-#print(df_My)
 
 #マルチ毎の交信数をJCC/WD/JCFのPDに転記する。地図をそれぞれで作成するもとデータ
 df_JCC.insert(5, 'Count', df_JCC['muniCd'].map(df_My.set_index('JCCmuniCd')['Count']))   
 df_WD.insert(5, 'Count', df_WD['muniCd'].map(df_My.set_index('JCCmuniCd')['Count']))   
 df_JCG.insert(5, 'Count', df_JCG['muniCd'].map(df_My.set_index('JCCmuniCd')['Count']))   
 
-#print(df_WD)
+
 
 #地図塗りつぶしデータ N03_007は国土地理院のデータの列名
 JCCmap_data = geo_data.merge(df_JCC, left_on = 'N03_007', right_on = 'muniCd', how = "left")
@@ -88,9 +101,9 @@ cmap = 'coolwarm'
 fig, ax = plt.subplots(1, 1, figsize=(10, 8))
 #コンテスト名を外に出す、File名と同じに
 ax.set_title('Multi Map', fontsize=18)
-#緯度経度指定で描画エリア設定。全国地図で大きすぎる場合にはマルチの範囲でgooglemapでもみて範囲を見て設定してください。
-#plt.xlim([139,141])
-#plt.ylim([35,37])
+#緯度経度指定で描画エリア設定。全国地図で大きすぎる、色が薄すぎる場合にはマルチの範囲でgooglemapでもみて範囲を見て設定してください。現在は関東地方にセットしてます。
+plt.xlim([138,141])
+plt.ylim([35,37])
 
 #実際に描画
 geo_data.plot(ax = ax, edgecolor='#444', facecolor='white', linewidth = 0.1) 
